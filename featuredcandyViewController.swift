55889533d4d83ed4e511ab9583f10f9874098ca8
Kaci Koizumi <koizumi3@hawaii.edu>
2023-10-31 13:56:10 -1000

    added second screen attached to button but still need to add images for all screens

---
+++ featuredcandyViewController.swift
@@ -0,0 +1,29 @@
+//
+//  featuredcandyViewController.swift
+//  halloweencandyreview
+//
+//  Created by Kaci Koizumi on 10/31/23.
+//
+
+import UIKit
+
+class featuredcandyViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ fourthViewController.swift
@@ -0,0 +1,36 @@
+//
+//  fourthViewController.swift
+//  halloweencandyreview
+//
+//  Created by Kaci Koizumi on 10/31/23.
+//
+
+import UIKit
+
+class fourthViewController: UIViewController {
+
+    @IBOutlet weak var featCandyLabel: UILabel!
+    @IBOutlet weak var candyDesc: UILabel!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        featCandyLabel.text = "Dum Dums"
+        candyDesc.text = "Dum Dums are a classic lollipop that come in an extreme variety of flavors."
+        
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
