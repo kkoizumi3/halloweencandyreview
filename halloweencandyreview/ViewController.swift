6b222d5f2d88d3abbe1a1e714d203f6e1bfb0f74
Kaci Koizumi <koizumi3@hawaii.edu>
2023-11-02 12:43:57 -1000

    small revisions made to part 1: added featured candy image and title on homepage

--- halloweencandyreview/ViewController.swift
+++ halloweencandyreview/ViewController.swift
@@ -10,13 +10,16 @@ import UIKit
 class ViewController: UIViewController {
     
     @IBOutlet weak var backgroundImage: UIImageView!
+    @IBOutlet weak var dumDumsFeatImage: UIImageView!
+    @IBOutlet weak var featCandyLabel: UILabel!
     
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        
+        featCandyLabel.text = "Dum Dums"
         backgroundImage.image = UIImage (named: "candy review cm161")
+        dumDumsFeatImage.image = UIImage (named: "dumdums")
         self.navigationItem.title = "Kaci's Candy Review"
     }
 
